6cfd5b2dc6b48aee783973cc0fac0b3697c68899
kritiz04 <kritisaxena013@gmail.com>
2025-06-24 23:00:47 +0530

    [Kriti].Add problems of my JavaConstructor on day-10

---
+++ JavaConstructor/BankAccount.java
@@ -0,0 +1,33 @@
+class BankAccount {
+    public int accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(int accountNumber, String accountHolder, double balance) {
+        super(accountNumber, accountHolder, balance);
+    }
+
+    public void displayInfo() {
+        System.out.println("Account Number: " + accountNumber + ", Holder: " + accountHolder);
+    }
+}
